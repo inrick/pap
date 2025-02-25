3f9f4607c4105ff5186d65ec0d56d953cb3e7334
Fabian Carlström <inrick@gmail.com>
2025-02-25 21:58:09 +0100

    Cleanup

--- part4/mathalt/math.go
+++ part4/mathalt/math.go
@@ -13,52 +13,45 @@ const collectStats = true
 type StatEntry struct{ InMin, InMax float64 }
 type Statistics struct{ Sin, Cos, Asin, Sqrt StatEntry }
 
+func collectStat(e *StatEntry, x float64) {
+	if collectStats {
+		e.InMin = min(x, e.InMin)
+		e.InMax = max(x, e.InMax)
+	}
+}
+
 // Initialized in init()
 var stats Statistics
 
 func init() {
 	if collectStats {
-		stats.Sin.InMin = math.Inf(1)
-		stats.Cos.InMin = math.Inf(1)
-		stats.Asin.InMin = math.Inf(1)
-		stats.Sqrt.InMin = math.Inf(1)
-		stats.Sin.InMax = math.Inf(-1)
-		stats.Cos.InMax = math.Inf(-1)
-		stats.Asin.InMax = math.Inf(-1)
-		stats.Sqrt.InMax = math.Inf(-1)
+		for _, e := range []*StatEntry{
+			&stats.Sin, &stats.Cos, &stats.Asin, &stats.Sqrt,
+		} {
+			e.InMin = math.Inf(1)
+			e.InMax = math.Inf(-1)
+		}
 	}
 }
 
 // Wrappers around math functions that also record max/min of input.
 func Sin(x float64) float64 {
-	if collectStats {
-		stats.Sin.InMin = min(x, stats.Sin.InMin)
-		stats.Sin.InMax = max(x, stats.Sin.InMax)
-	}
+	collectStat(&stats.Sin, x)
 	return math.Sin(x)
 }
 
 func Cos(x float64) float64 {
-	if collectStats {
-		stats.Cos.InMin = min(x, stats.Cos.InMin)
-		stats.Cos.InMax = max(x, stats.Cos.InMax)
-	}
+	collectStat(&stats.Cos, x)
 	return math.Cos(x)
 }
 
 func Asin(x float64) float64 {
-	if collectStats {
-		stats.Asin.InMin = min(x, stats.Asin.InMin)
-		stats.Asin.InMax = max(x, stats.Asin.InMax)
-	}
+	collectStat(&stats.Asin, x)
 	return math.Asin(x)
 }
 
 func Sqrt(x float64) float64 {
-	if collectStats {
-		stats.Sqrt.InMin = min(x, stats.Sqrt.InMin)
-		stats.Sqrt.InMax = max(x, stats.Sqrt.InMax)
-	}
+	collectStat(&stats.Sqrt, x)
 	return math.Sqrt(x)
 }
 
